de8bf1c05f3fcf6d08053cf944581d531056b9b5
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2021-08-30 14:34:13 +0300

    TIMEOUT RPC status when LwM2M request is timed oud

--- common/transport/lwm2m/src/main/java/org/thingsboard/server/transport/lwm2m/server/rpc/RpcDownlinkRequestCallbackProxy.java
+++ common/transport/lwm2m/src/main/java/org/thingsboard/server/transport/lwm2m/server/rpc/RpcDownlinkRequestCallbackProxy.java
@@ -41,8 +41,6 @@ import org.thingsboard.server.gen.transport.TransportProtos;
 import org.thingsboard.server.transport.lwm2m.server.client.LwM2mClient;
 import org.thingsboard.server.transport.lwm2m.server.downlink.DownlinkRequestCallback;
 
-import java.util.concurrent.TimeoutException;
-
 public abstract class RpcDownlinkRequestCallbackProxy<R, T> implements DownlinkRequestCallback<R, T> {
 
     private final TransportService transportService;
@@ -83,7 +81,7 @@ public abstract class RpcDownlinkRequestCallbackProxy<R, T> implements DownlinkR
 
     @Override
     public void onError(String params, Exception e) {
-        if (e instanceof TimeoutException) {
+        if (e instanceof java.util.concurrent.TimeoutException || e instanceof org.eclipse.leshan.core.request.exception.TimeoutException) {
             transportService.process(client.getSession(), this.request, RpcStatus.TIMEOUT, TransportServiceCallback.EMPTY);
         } else if (!(e instanceof ClientSleepingException)) {
             sendRpcReplyOnError(e);
